a1139ef76375e963038fa3d668b3cb743d77ac76
Craig Ambrose <craig@craigambrose.com>
2009-03-25 10:59:21 +1300

    removed unused files now that rails handles app path loading in plugin directories

--- init.rb
+++ init.rb
@@ -4,4 +4,3 @@ require 'models/plugin_instance.rb'
 
 require File.expand_path(dir + '/rails_extensions/rails_extensions')
 require File.expand_path(dir + '/ruby_extensions/ruby_extensions')
-
--- lib/rails_extensions/2-3-2/rails_extensions.rb
+++ lib/rails_extensions/2-3-2/rails_extensions.rb
@@ -1,8 +1,5 @@
 dir = File.expand_path(File.dirname(__FILE__))
 
-require File.expand_path(dir + '/plugin')
 require File.expand_path(dir + '/route_set')
 require File.expand_path(dir + '/route_set_mapper')
 require File.expand_path(dir + '/url_rewriter')
-require File.expand_path(dir + '/layout')
-require File.expand_path(dir + '/action_view')
